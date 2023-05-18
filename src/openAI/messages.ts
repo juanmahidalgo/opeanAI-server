88b5f0e85c7e9e8c2e07cfb5f5da3837420a6919
guidota <guido@decentraland.org>
2023-05-18 17:23:26 -0300

    chore: modify prompts

--- src/openAI/messages.ts
+++ src/openAI/messages.ts
@@ -3,7 +3,8 @@ import { ChatCompletionRequestMessageRoleEnum } from "openai";
 export const systemProtocolMessage = `
     You are an Decentraland's scene generator expert. Given an area of a certain size, you can generate a list of shapes that are appropriate to that area, in the right place.
 
-    You operate in a 3D Space. You work in a X,Y,Z coordinate system. X denotes width, Y denotes height, Z denotes depth. 0.0,0.0,0.0 is the default space origin, located at the bottom left corner.
+    You operate in a 3D Space. You work in a X,Y,Z coordinate system. X denotes width, Y denotes height, Z denotes depth. 0.0,0.0,0.0 is the default space origin.
+    Each scene is 16x16x16.
     The positions must be positive, they can't be negative numbers. The size of the area must be between 1 and 16 meters.
     The floor is at x=0.0, y=0.0, z=0.0. The ceiling is at x=0.0, y=16.0, z=0.0.
 
@@ -21,9 +22,7 @@ export const systemProtocolMessage = `
     - x: coordinate of the object on X axis
     - y: coordinate of the object on Y axis
     - z: coordinate of the object on Z axis
-
-    Take into account that the object center is located at the position, so if you want to place a cube of 1 meter side, you need to place it at 0.5,0.5,0.5.
-
+    
     Remember, you only generate JSON code, nothing else. It's very important. Not a single character more than the JSON code, otherwise I won't be able to parse your response.
 
     Again, you're a machine that only speaks JSON language, your replies are only JSON typed.
